546ea98538b25b729bef529af8cef63a38fba064
Andrew Thomas Porter <caritasdedeus@gmail.com>
2019-11-27 15:05:43 -0500

    Update main for testing (memory manager)

--- src/modulus.c
+++ src/modulus.c
@@ -2,59 +2,30 @@
  ============================================================================
  Name        : Modulus.c
  Author      : Andrew Thomas Porter (AMDG)
- Version     : 0.0.1
- Copyright   : License
+ Version     : 1.2.1
+ Copyright   : See LICENSE.txt
  Description : Modulus language compiler
  ============================================================================
  */
-
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 
+// make other libraries use our memory manager (overwrites free as well)
+#define MEMORY_MALLOC_OVERWRITE true
+// enable debug message printing
+#define R_DEBUG true
+// set memory unit to use dynamic allocations instead of a single static allocation
+#define DYNAMIC_HEAP true
+#include "memory.h"
 #include "debug.h"
 #include "compiler.h"
 
 
-// the ab language with productions S -> aSb and S -> ab
-struct node* mdu_ab_lang(struct node* origin, struct symbol* input) {
-	static struct node result;
-	// principles:
-	// a -> a : true;
-	// b -> b : true;
-	// a -> b : true;
-	// b -> a : false
-	char x = (char)(intptr_t) origin->x->y;
-	char y = (char)(intptr_t) input->y;
-
-	if ((x == 'a' && y == 'a') || (x == 'b' && y == 'b')) { // a -> a or b -> b
-		result.origin = origin;
-		result.x = input;
-		return &result;
-	} else {
-		if ((x == 'a' && y == 'b')) { // a -> b
-			result.origin = origin;
-			result.x = input;
-			return &result;
-		} else { // b -> a or erroneous case
-			return NULL;
-		}
-	}
-}
-
-int main(int argc, char* argv[]) {
-	mdu_debug_info("Running...\n");
-	//if (argc > 0) {
-		//char* value = argv[0];
-		char* value = "aaaaabbbbb";
-		size_t length = strlen(value);
-
-		struct symbol* input = mdu_str_input(value, length);
-		struct symbol* result = mdu_match_symbol_subset(&mdu_ab_lang, input);
-		mdu_debug_infof(MDU_SUCCESS, "[%s] number of symbols in input: %d\n", __func__, result->length);
-	//} else {
-	//	puts("No input given. Exiting...\n");
-	//}
+int main(int argc, char *argv[]) {
+	r_debug_info(__func__, "Running...\n");
+	m_sysheap_create(64, 128);
 	return 0;
 }
