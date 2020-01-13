89c8ae7672bdb098a7328a9d6b9f8970827c6e08
AMDG <caritasdedeus@gmail.com>
2020-01-13 10:00:15 -0500

    Update prototype.u with syntax candidate and language principles

---
+++ src/language/dedicated_parser.h
@@ -0,0 +1,15 @@
+/*
+ * # dedicated_parser.h
+ * ## Created:
+ * January 10, A.D. 2020
+ * ## Author:
+ * Andrew Thomas Porter [<caritasdedeus@gmail.com>](mailto:caritasdedeus@gmail.com)
+ *
+ * Copyright &copy; 2020 Christi Crucifixi, LLC. All rights reserved.
+ */
+
+#ifndef CATHOLICUS_DEDICATED_PARSER_H
+#define CATHOLICUS_DEDICATED_PARSER_H
+
+
+#endif //CATHOLICUS_DEDICATED_PARSER_H
