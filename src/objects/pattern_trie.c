47c5865f47eaf606446def45cde06f6150bc048c
Andrew Porter <caritasdedeus@gmail.com>
2020-06-20 21:48:05 -0400

    Rename permutation to pattern_trie

--- src/objects/pattern_trie.c
+++ src/objects/pattern_trie.c
@@ -1,13 +1,13 @@
 /*
- * Module: permutation
- * File: permutation.c
+ * Module: pattern_trie
+ * File: pattern_trie.c
  * Created:
  * April 08, 2020
  * Author: Andrew Porter [<caritasdedeus@gmail.com>](mailto:caritasdedeus@gmail.com)
  *
  * Copyright &copy; 2020 Christi Crucifixi, LLC. All rights reserved.
  */
-#include "permutation.h"
+#include "pattern_trie.h"
 
-permutation *p_next(permutation *reference, permutation *in, permutation *out) {
+pattern_trie *p_next(pattern_trie *reference, pattern_trie *in, pattern_trie *out) {
 }
---
+++ src/objects/pattern_trie.h
@@ -0,0 +1,32 @@
+/*
+ * Module: pattern_trie
+ * File: pattern_trie.h
+ * Created:
+ * February 25, 2020
+ * Author: Andrew Porter [<caritasdedeus@gmail.com>](mailto:caritasdedeus@gmail.com)
+ *
+ * Copyright &copy; 2020 Christi Crucifixi, LLC. All rights reserved.
+ */
+
+#ifndef PROJECT_AQUINAS_PATTERN_TRIE_H
+#define PROJECT_AQUINAS_PATTERN_TRIE_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "binary_trie.h"
+
+typedef union node {
+
+} node;
+typedef struct pattern_trie {
+	struct pattern_trie *children;
+	uword order;
+} pattern_trie;
+
+pattern_trie p_read(FILE *in);
+
+void p_write(pattern_trie in, FILE *out);
+
+#endif //PROJECT_AQUINAS_PATTERN_TRIE_H
