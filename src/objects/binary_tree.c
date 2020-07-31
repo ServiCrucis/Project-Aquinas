2d8bf86b1f00674fc6cd955c16ef401ea8e3b2c1
Andrew Porter <caritasdedeus@gmail.com>
2020-07-31 13:18:14 -0400

    Rename module binary_trie to binary_tree

---
+++ src/objects/binary_tree.c
@@ -0,0 +1,59 @@
+/*
+ * Module: binary_tree
+ * File: binary_tree.c
+ * Created:
+ * May 20, 2020
+ * Author: Andrew Porter [<caritasdedeus@gmail.com>](mailto:caritasdedeus@gmail.com)
+ *
+ * Copyright &copy; 2020 Christi Crucifixi, LLC. All rights reserved.
+ */
+
+#include "platform.h"
+#include "binary_tree.h"
+#include "bit_math.h"
+
+
+binary_tree *binary_tree_create(uword nodes) {
+    binary_tree *tree = calloc((2u << sigbits(nodes)), sizeof(uword));
+    tree->nodes = nodes;
+    return tree;
+}
+
+void binary_tree_free(binary_tree *restrict tree) {
+
+}
+
+static inline binode *b_traverse(binode *node, uword address) {
+traverse:
+    node = (address & 1) ? node->one : node->zero;
+    address >>= 1;
+
+    if (address > 0)
+        return node;
+    else
+        goto traverse;
+}
+
+uword binary_tree_get(binary_tree *restrict tree, uword address) {
+    return b_traverse((binode *) tree, address)->value;
+}
+
+void binary_tree_set(binary_tree *restrict tree, uword address, uword value) {
+    b_traverse((binode *) tree, address)->value = value;
+}
+
+uword *b_get_all(binary_tree *restrict tree, uword *addresses, uword length) {
+    uword *values = malloc(sizeof(uword) * length);
+    binode *node;
+iterate_addresses:
+    for (ubyte i = 0; i < length; i++) {
+        node = b_traverse((binode *) tree, addresses[i]);
+        values[i] = node->value;
+    }
+}
+
+void b_set_all(binary_tree *restrict tree, pair *values, uword length) {
+    binode *node;
+    pair *pair;
+
+}
---
+++ src/objects/binary_tree.h
@@ -0,0 +1,51 @@
+/*
+ * Module: binary_tree
+ * File: binary_tree.h
+ * Created:
+ * May 20, 2020
+ * Author: Andrew Porter [<caritasdedeus@gmail.com>](mailto:caritasdedeus@gmail.com)
+ *
+ * Copyright &copy; 2020 Christi Crucifixi, LLC. All rights reserved.
+ */
+
+#ifndef PROJECT_AQUINAS_BINARY_TREE_H
+#define PROJECT_AQUINAS_BINARY_TREE_H
+
+#include "platform.h"
+#include "bit_math.h"
+
+typedef struct binode {
+    struct binode *zero, *one;
+    uword value;
+} binode;
+
+
+// A binary_tree implementation utilizing uword integer values aligned to data model
+typedef struct binary_tree {
+    binode *zero, *one;
+    uword value;
+	uword nodes;
+} binary_tree;
+
+binary_tree *binary_tree_create(uword nodes);
+
+void binary_tree_free(binary_tree *tree);
+
+/*
+ * Gets the value at the given address in the tree.
+ *
+ * A bitmask is generated based on the number of nodes in the tree to handle buffer overflow.
+ */
+uword binary_tree_get(binary_tree *restrict tree, uword address);
+/*
+ * Sets the value at the given address in the tree to the given value.
+ *
+ * A bitmask is generated based on the number of nodes in the tree to handle buffer overflow.
+ */
+void binary_tree_set(binary_tree *restrict tree, uword address, uword value);
+
+uword *b_get_all(binary_tree *restrict tree, uword *addresses, uword length);
+
+void b_set_all(binary_tree *restrict tree, pair *values, uword length);
+
+#endif //PROJECT_AQUINAS_BINARY_TREE_H
