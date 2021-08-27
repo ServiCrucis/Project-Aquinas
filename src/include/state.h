c98e16805a31507c0b8933bdf810f297cb1d17f7
AMDG <caritasdedeus@gmail.com>
2021-08-27 12:46:06 -0400

    Remove __attribute__((format_arg(2))) from functions in state.h
    I'll have to look into this one a bit more before I can use it if necessary at all.

--- src/include/state.h
+++ src/include/state.h
@@ -58,16 +58,13 @@ static inline void clear_context() {
     _global_context = _global_previous_context;
 }
 
-__attribute__((format_arg(2)))
 void info(char const *restrict fn_name, char const *information, ...);
 
-__attribute__((format_arg(2)))
 void infof(char const *restrict fn_name, char const *restrict information, ...);
 
-__attribute__((format_arg(2)))
 void warnf(char const *restrict fn_name, char const *restrict warning, ...);
 
-__attribute__((noreturn, format_arg(2)))
+__attribute__((noreturn))
 void fatalf(char const *restrict fn_name, char const *restrict error_message, ...);
 
 #endif /* PROJECT_AQUINAS_DEBUG_H */
