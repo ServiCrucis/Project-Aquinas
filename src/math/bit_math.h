32853e112a4dc5d7f0dc56c329ecd1947d22013b
AMDG <caritasdedeus@gmail.com>
2021-10-15 22:59:01 -0400

    Push latest before leaving for vacation

--- src/math/bit_math.h
+++ src/math/bit_math.h
@@ -68,10 +68,10 @@ typedef struct pair {
  */
 #define shift(value, a) ( (!a ? value : (a < 0 ? value >> a : value << a)) )
 
-
 /*
  * Zeroes the value at the given location using bit math.
  */
+__attribute__((hot, pure))
 static inline void zero(uqword *restrict in) {
     *in ^= *in;
 }
@@ -90,7 +90,7 @@ static inline void zero(uqword *restrict in) {
 /*
  * Generates a mersenne number and shifts by `offset` bits relative to the lsb using the `shift` macro function.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword mask(register uqword bits, register qword offset) {
     return shift(((1ull << bits) - 1), offset);
 }
@@ -99,7 +99,7 @@ static inline uqword mask(register uqword bits, register qword offset) {
  * Generates a bit filter from `value` by masking `bits` bits at `offset` offset relative to the lsb using the
  * `shift` macro function.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword filter(register uqword value, register uqword bits, register qword offset) {
     return value & mask(bits, offset);
 }
@@ -110,7 +110,7 @@ static inline uqword sigbits(uqword);
 /*
  * Compute number of significant base 10 digits in a given base 2 qword
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword digits(register uqword bit_string) {
     // ln(10) / ln(2) ~= 3.3219280948873623478703194294894
     const udqword numerator   = 10000ull;
@@ -122,7 +122,7 @@ static inline uqword digits(register uqword bit_string) {
 /*
  * Zeroes all bits above the given bit index in src.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword zero_high_bits(register uqword src, register uqword index) {
     #if ARCH == ARCH_X86_64
     return __x64_bzhiq(src, src, index);
@@ -137,7 +137,7 @@ static inline uqword zero_high_bits(register uqword src, register uqword index) 
 /*
  * Count the number of leading zeroes in bit_string.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword cntlz(register uqword bit_string) {
     #if ARCH == ARCH_X86_32
     return __x86_lzcnt(bit_string);
@@ -158,7 +158,7 @@ static inline uqword cntlz(register uqword bit_string) {
 /*
  * Count the number of trailing zeroes in bit_string.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword cnttz(register uqword bit_string) {
     #if ARCH == ARCH_X86_32
     return __x86_tzcnt(bit_string);
@@ -179,7 +179,7 @@ static inline uqword cnttz(register uqword bit_string) {
 /*
  * Count the number of ones in a bit_string
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword ones(register uqword bit_string) {
     #if ARCH == ARCH_X86_32
     return __x86_popcnt(bit_string);
@@ -198,12 +198,184 @@ static inline uqword ones(register uqword bit_string) {
 }
 
 static inline uqword log2i(register uqword);
+
+// computes 6-bit divide; returns ~0 for y=0 and 0 for y > 64
+// intended to be used for computing how many times we must shift.
+static inline uqword udiv6(register uqword x, register ubyte y) {
+    switch (y) {
+        case 0:
+            return ~0;
+        case 1:
+            return x;
+        case 2:
+            return x >> 1;
+        case 3:
+            return x / 3;
+        case 4:
+            return x >> 2;
+        case 5:
+            return x / 5;
+        case 6:
+            return x / 6;
+        case 7:
+            return x / 7;
+        case 8:
+            return x >> 3;
+        case 9:
+            return x / 9;
+        case 10:
+            return x / 10;
+        case 11:
+            return x / 11;
+        case 12:
+            return x / 12;
+        case 13:
+            return x / 13;
+        case 14:
+            return x / 14;
+        case 15:
+            return x / 15;
+        case 16:
+            return x >> 4;
+        case 17:
+            return x / 17;
+        case 18:
+            return x / 18;
+        case 19:
+            return x / 19;
+        case 20:
+            return x / 20;
+        case 21:
+            return x / 21;
+        case 22:
+            return x / 22;
+        case 23:
+            return x / 23;
+        case 24:
+            return x / 24;
+        case 25:
+            return x / 25;
+        case 26:
+            return x / 26;
+        case 27:
+            return x / 27;
+        case 28:
+            return x / 28;
+        case 29:
+            return x / 29;
+        case 30:
+            return x / 30;
+        case 31:
+            return x / 31;
+        case 32:
+            return x >> 5;
+        case 33:
+            return x / 33;
+        case 34:
+            return x / 34;
+        case 35:
+            return x / 35;
+        case 36:
+            return x / 36;
+        case 37:
+            return x / 37;
+        case 38:
+            return x / 38;
+        case 39:
+            return x / 39;
+        case 40:
+            return x / 40;
+        case 41:
+            return x / 41;
+        case 42:
+            return x / 42;
+        case 43:
+            return x / 43;
+        case 44:
+            return x / 44;
+        case 45:
+            return x / 45;
+        case 46:
+            return x / 46;
+        case 47:
+            return x / 47;
+        case 48:
+            return x / 48;
+        case 49:
+            return x / 49;
+        case 50:
+            return x / 50;
+        case 51:
+            return x / 51;
+        case 52:
+            return x / 52;
+        case 53:
+            return x / 53;
+        case 54:
+            return x / 54;
+        case 55:
+            return x / 55;
+        case 56:
+            return x / 56;
+        case 57:
+            return x / 57;
+        case 58:
+            return x / 58;
+        case 59:
+            return x / 59;
+        case 60:
+            return x / 60;
+        case 61:
+            return x / 61;
+        case 62:
+            return x / 62;
+        case 63:
+            return x / 63;
+        case 64:
+            return x >> 6;
+        default:
+            return 0;
+    }
+}
+
+/*
+ * Compares a power of two encoded as exponent in `exp` to a value in `value` by computing their difference using
+ * bit math.
+ */
+static inline ubyte cmp2pN8(register ubyte exp, register uqword value) {
+    const register udqword a = (1 << (exp % bitwidth(a)));
+    const register udqword b = value << ((exp - log2i(value)) % bitwidth(b));
+    return (a - b);
+}
+
+/*
+ * Evaluates 2**exp mod modulus in O(log n) time using bit math.
+ */
+__attribute__((hot,const))
+static inline udqword umod2pN8(register ubyte exp, register uqword modulus) {
+    if (exp < modulus) {
+        return (1 << exp);
+    } else if (exp == modulus) {
+        return 0;
+    } else {
+        // modular accumulator
+        uqword a = 0;
+        // first power of two greater than the modulus' power of two
+        uqword pow2_gt_modulus = log2i(modulus) + 1;
+        // modular decrementor
+        uqword d = 0;
+    algorithm_start:
+    
+    }
+}
+
 /*
  * Evaluates fast modulus as a mod b to machine precision using bit math.
  */
 __attribute__((const))
-static inline uqword umodq(register uqword a, register uqword b) {
-    // 1. compute a mod b * 2**(log_2(a) - log_2(b)
+static inline uqword umodq(register uqword x, register uqword modulus) {
+    
+
     
 }
 
@@ -213,7 +385,7 @@ static inline uqword umodq(register uqword a, register uqword b) {
  *
  * The input is two integers. The returned value is a fixed-point u32.32 value.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword square_wave(register uqword period, register uqword time) {
     // implements sgn( (time mod period) - (period - 1)/2 )
     // a = (time mod period)
@@ -260,29 +432,28 @@ static inline ubyte square_wave_ext_infty(register udqword period, register udqw
 __attribute__((noinline))
 static inline uqword urcpq(register uqword divisor) {
     // used with the digit_bitwidth parameter of functions defined here
-    const static ubyte digit_width_quotient_table = {
-      0
+    const static ubyte    digit_width_quotient_table = {
+            0
     };
-    const register uqword base = divisor;
-    const register bool divisor_is_power_of_two = ones(divisor) == 1;
-
+    const register uqword base                       = divisor;
+    const register bool   divisor_is_power_of_two    = ones(divisor) == 1;
+    
     if (divisor_is_power_of_two) {
-       return (1ull << (bitwidth(uqword) - 1)) >> log2i(divisor);
+        return (1ull << (bitwidth(uqword) - 1)) >> log2i(divisor);
     } else {
         // initialize base `divisor` registers
         register udqword erax, erbx, ercx, erdx;
         // define base `divisor` operations necessary for computing reciprocals.
         // apparently inner functions are a security hazard, but whatever--C is a means to an end
-
+        
         // digit shift in native base
-        __attribute__((hot,const,always_inline))
+        __attribute__((hot, const, always_inline))
         udqword base_div_shift(udqword reg, sbyte digits, ubyte digit_bitwidth) {
             return shift(reg, digits / digit_bitwidth);
         }
         // 2**n in native base
-        __attribute__((hot,const,always_inline))
+        __attribute__((hot, const, always_inline))
         udqword base_div_pow2(udqword reg, ubyte exponent, ubyte digit_bitwidth) {
-
         }
     }
     
@@ -293,9 +464,6 @@ static inline uqword urcpq(register uqword divisor) {
     const uqword B = 0x2B9EFB << (31 - sigbits(0x2B9EFB));
     // (-) 2.096641
     const uqword C = 0x218BD7 << (31 - sigbits(0x218BD7));
-    
-    
-    
 }
 
 #ifndef BIT_MATH_USE_HW_MUL
@@ -348,16 +516,15 @@ static inline udqword umulq(register uqword multiplicand, register uqword multip
 __attribute__((noinline))
 static inline uqword udivq(register uqword dividend, register uqword divisor) {
     return (uqword)
-    ((((udqword) urcpq(divisor)) << bitwidth(urcpq(divisor))) *
-    ((udqword) dividend << (bitwidth(urcpq(divisor)))) >>
-    bitwidth(urcpq(divisor)))
-    ;
+            ((((udqword) urcpq(divisor)) << bitwidth(urcpq(divisor))) *
+             ((udqword) dividend << (bitwidth(urcpq(divisor)))) >>
+                                                                bitwidth(urcpq(divisor)));
 }
 
 /*
  * Compute the number of significant bits in the given uqword
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword sigbits(register uqword bit_string) {
     #if defined(__GNUC__)
       #if DATA_MODEL == LLP64 || DATA_MODEL == ILP64 || DATA_MODEL == SILP64
@@ -426,7 +593,7 @@ static inline uqword sigbitsn(register uqword *bit_string, register size_t words
 /*
  * Compute 2 to the power of exponent using integer bit math with truncated results for overflow.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword pow2i(register uqword exponent) {
     return 1ull << exponent;
 }
@@ -563,7 +730,7 @@ static inline uqword powni(register uqword base, register uqword exponent) {
 /*
  * Compute log base 2 of the given bit string using integer bit math.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword log2i(register uqword bit_string) {
     return sigbits(bit_string) - 1ull;
 }
@@ -604,7 +771,7 @@ static inline uqword lni(register uqword bit_string) {
 /*
  * Computes the value of a single bit at a given digit offset from the right.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword get_digit2i(uqword value, uqword digit) {
     return (value >> digit) & 1;
 }
@@ -621,7 +788,7 @@ static inline uqword get_digit10i(uqword value, uqword digit) {
 /*
  * Doubles the given value using bit math.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword dbl(register uqword const value) {
     return value << 1u;
 }
@@ -629,7 +796,7 @@ static inline uqword dbl(register uqword const value) {
 /*
  * Halves the given value using bit math.
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword hlv(register uqword const value) {
     return value >> 1u;
 }
@@ -639,7 +806,7 @@ static inline uqword hlv(register uqword const value) {
 /*
  * Compute if given value is within the range [min, max].
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline bool in_range(register uqword const min, register uqword const max, register uqword const value) {
     return (min <= value) & (value <= max);
 }
@@ -647,7 +814,7 @@ static inline bool in_range(register uqword const min, register uqword const max
 /*
  * Compute if given value is within the range [min, max).
  */
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline bool in_buffer(register uqword const min, register uqword const max, register uqword const value) {
     return (min <= value) & (value < max);
 }
@@ -685,12 +852,13 @@ static inline uqword get_bita(uqword const *restrict bitarray, uqword const word
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-__attribute__((hot,const))
+
+__attribute__((hot, const))
 static inline uqword get_bit(uqword bit_string, ubyte bit) {
     return (bit_string >> bit) & 1ull;
 }
 
-__attribute__((hot,const))
+__attribute__((hot, const))
 static inline uqword set_bit(uqword bit_string, ubyte bit, ubyte value) {
     bit_string ^= (-value ^ bit_string) & (1ull << bit);
     return bit_string;
