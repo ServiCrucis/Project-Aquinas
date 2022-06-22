384e82305d2a4e96ec4707e92f34c953111db47f
AMDG <caritasdedeus@gmail.com>
2022-06-22 17:54:51 -0400

    The Heap is Dead Long live localized infinite address memory model!

---
+++ src/include/memory.c
@@ -0,0 +1,21 @@
+/*
+ * Module: memory
+ * File: memory.c
+ * Created:
+ * May 03, 2021
+ * Author: Andrew Porter [<caritasdedeus@gmail.com>](mailto:caritasdedeus@gmail.com)
+ *
+ * Copyright &copy; 2021 Christi Crucifixi, LLC. All rights reserved.
+ *
+ * License: See LICENSE.txt
+ */
+
+#include <malloc.h>
+#include "state.h"
+#include "bit_math.h"
+#include "memory.h"
+
+
+
+
+
